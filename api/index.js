1d1dae849fbf62729515cf104125128982cf431c
ladikpe <linuxdikpe@gmail.om>
2024-09-09 11:45:42 +0000

    another commit

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Example route
+// Example route yes  of  
 app.get('/', (req, res) => {
   res.send('Welcome to the E-commerce API!');
 });
